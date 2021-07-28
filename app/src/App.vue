596ba624ac5b23e52c31ad4b05717ff952e7fb57
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-07-28 15:15:37 +0900

    added upload photo function

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,6 +4,8 @@
       <ShowingPhotos />
       <UploadingItems />
       <Upload />
+      <upload v-model="picture" />
+      <img :src="picture" />
     </div>
     <div class="content">
       <div class="inner">
@@ -31,6 +33,7 @@ export default {
     return {
       text3: "Put Item in Your Closet",
       text4: "View My Closet",
+      picture: null,
     };
   },
 };
@@ -44,5 +47,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  color: #2c2d30;
+  font-size: 1.6rem;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
 }
 </style>
--- app/src/components/Upload.vue
+++ app/src/components/Upload.vue
@@ -1,11 +1,69 @@
 <template>
   <div>
     Upload photo
+    <label v-if="!value" class="upload-content-space user-photo default">
+      <input ref="file" class="file-button" type="file" @change="upload" />
+      Upload!
+    </label>
+
+    <div v-if="value" class="uploaded">
+      <label class="upload-content-space user-photo">
+        <input ref="file" class="file-button" type="file" @change="upload" />
+        <img class="user-photo-image" :src="value" />
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Upload",
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      file: null,
+    };
+  },
+  methods: {
+    async upload(event) {
+      const files = event.target.files || event.dataTransfer.files;
+      const file = files[0];
+
+      if (this.checkFile(file)) {
+        const picture = await this.getBase64(file);
+        this.$emit("input", picture);
+      }
+    },
+    getBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    },
+    checkFile(file) {
+      let result = true;
+      const SIZE_LIMIT = 5000000; // 5MB
+      // キャンセルしたら処理中断
+      if (!file) {
+        result = false;
+      }
+      // jpeg か png 関連ファイル以外は受付けない
+      if (file.type !== "image/jpeg" && file.type !== "image/png") {
+        result = false;
+      }
+      // 上限サイズより大きければ受付けない
+      if (file.size > SIZE_LIMIT) {
+        result = false;
+      }
+      return result;
+    },
+  },
 };
 </script>
